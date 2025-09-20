0b5f9a2d096d0312f625b016042be94525a59c94
Erusha Sandushan <erushasandushan@gmail.com>
2025-09-20 22:55:52 +0530

    beta v0.2

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Cafeteria-System
+
 A C++ and SQL-based cafeteria management system built as a learning project to strengthen my C++ skills and practice database integration.
+Started Learning C++ in September 2025.
+
+Username : `admin` / `manager`
+Password : `password`
 
 ![](./Imgs/Initial_stage.png)
 
 ### TO DO
 - Menu System 
    - View available food items. [ DONE ]
-   - Add new items (admin mode).
+   - Add new items (admin mode) [ PENDING ].
 
 - Order System
 
@@ -16,12 +21,20 @@ A C++ and SQL-based cafeteria management system built as a learning project to s
 
 - Reports (Admin Panel)
 
-  - Show all orders of the day.
-  - Find most popular item.
-  - Show total sales.
-- Login (Admins only) 
-  - TODO : More secure password encryption. 
-  <!--
+  - Show all orders of the day. [ MAYBE :) ]
+  - Find most popular item. [ DONE ]
+  - Show total sales. [ DONE ]
+- Login (Admins only) [ DONE ]
+  - More secure password encryption [ PENDING ] 
+  - User Management [ PENDING ]
+  
+- Web UI
+  - Design REST API backend in C++ (Crow) [ PENDING ]
+  - Connect SQLite database to backend for live data retrieval and updates [ PENDING ]
+  - Develop Web UI (frontend) [ PENDING ]
+  - Admin Dashboard [ PENDING ]
+
+<!--
   
   DB Structure 
     - Menu items
--- main.cpp
+++ main.cpp
@@ -35,11 +35,11 @@ class DB
         void read_data()
         {   connect();
             char* zErrMsg = 0;
-            char* sql = "SELECT * from Menu;";
+            std::string sql = "SELECT * from Menu;";
             int rc;
             std::cout <<"Item            Name                    Price \n";
             std::cout<<"--------------------------------------------------------------\n";
-            rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+            rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
             std::cout<<"--------------------------------------------------------------\n";   
             return;
         }
@@ -82,6 +82,89 @@ class DB
             std::cout << "Total Bill : " << total <<std::endl;
 
         }
+};
+
+class Admin
+/*
+Date        : 20/08/2025
+Description : This is admin panel that manages the DB & Sales reports
+
+Wanna do : 
+    Make Code Advanced than main.cpp
+
+*/
+
+{
+    public:
+        sqlite3* db;
+        std::string password_db;
+
+        int banner()
+        {
+         
+            std::cout << "\n=============================================================" << std::endl;
+            std::cout << "                 Welcome to  Admin Panel " << std::endl;
+            std::cout << "=============================================================\n" << std::endl;
+            return 0;
+        }
+        int connect()
+        {
+            int exit = 0;
+            exit = sqlite3_open("./DB/cafeteria.db",&db);
+                if (exit) {
+                std::cerr << "[-] Error opening DB " << sqlite3_errmsg(db) << std::endl;
+                return (-1);
+            }
+            return 0;
+        }
+        static int callback(void *data, int argc, char **argv, char **azColName)
+        {
+            for(int i=0;i<argc;i++)
+            {
+                std::cout << "\t\t" << argv[i];
+            }
+            std::cout << "\n";
+            return 0;
+        }
+        int GetReport()
+        {
+            connect();
+            char* emsg = 0;
+            std::string sql = "SELECT m.Item, m.Name FROM Orders o JOIN Menu m ON o.Item = m.Item GROUP BY m.Item, m.Name ORDER BY COUNT(o.Item) DESC LIMIT 3;";
+            int rc;
+            rc = sqlite3_exec(db, sql.c_str(),callback, 0, &emsg);
+            return 0;
+        }
+        static int callback_sales(void *data, int argc, char **argv, char **azColName)
+        {
+            std::cout << "\nTotal Sales : " << argv[0] << "\n\n";
+            return 0;
+        }
+        int GetSales(){
+            connect();
+            char* emsg = 0;
+            std::string sql = "SELECT sum(TotalBill) from Orders;";
+            int rc;
+            rc = sqlite3_exec(db, sql.c_str(),callback_sales, 0, &emsg);
+            return 0;
+        }
+        static int callback_auth(void *data, int argc, char **argv, char **azColName)
+        {
+            std::string* pass_db = static_cast<std::string*>(data); 
+            *pass_db = argv[0];
+            return 0;
+        }
+        void read_password(std::string user)
+        {   
+            connect();
+            char* zErrMsg = 0;
+            std::string sql = "SELECT Password from Users WHERE Username = '"+ user +"';"; //vuln for sqli :)
+            int rc;
+            rc = sqlite3_exec(db, sql.c_str(),callback_auth, &password_db, &zErrMsg);
+            return;
+        }
+
+        
         std::string md5Encrypt(const std::string& input) {
             unsigned char digest[EVP_MAX_MD_SIZE];
             unsigned int digest_len = 0;
@@ -97,13 +180,30 @@ class DB
                 ss << std::hex << std::setw(2) << std::setfill('0') << (int)digest[i];
             return ss.str();
         }
-};
+
+        bool auth(std::string user,std::string user_pass)
+        {
+            read_password(user);
+            std::string enc_pass = md5Encrypt(user_pass);
+            if( enc_pass == password_db )
+            {
+                return 1;
+            }else{
+                return 0;
+            }
+            
+        }
+
+    };
+
 class Cafeteria 
 {
     private:
         DB& database;
+        Admin& admin;
     public:
-        Cafeteria(DB& db) : database(db) {}
+        //Cafeteria(DB& db) : database(db) {}
+        Cafeteria(DB& db, Admin& a) : database(db), admin(a) {}
         void banner()
             {
                 std::cout << "=============================================================" << std::endl;
@@ -119,7 +219,41 @@ class Cafeteria
                 std::cin >> uname;
                 std::cout << "Password : ";
                 std::cin >> pass;
-                std::cout << uname<<pass;
+                if (admin.auth(uname,pass))
+                {
+                    std::cout << "\033[2J\033[1;1H";
+                    admin.banner();
+                    // Report goas here
+                    std::cout << "\nSelect below options\n1.Top 3 Sold Items \n2.Total Sales \n3.Update Menu \n";
+                    int answer;
+                    std::cout <<"> ";
+                    std::cin >> answer;
+                    if(std::cin.fail()){
+                    std::cout << "Not a number";
+                    exit(1);
+                    }else if(answer == 1){
+                        std::cout << "----------------------------------------------------------";
+                        std::cout <<"\n\t  Item Code  \t\tName\n" ;
+                        std::cout << "----------------------------------------------------------\n";
+                        admin.GetReport();
+                        std::cout << "----------------------------------------------------------";
+                    }else if(answer == 2 )
+                    {
+                        admin.GetSales();
+                    }else if(answer == 3 )
+                    {
+                        std::cout << "\nUpdate option under construction.\nThank you for your patience!\n\n";
+                    }else{
+                        std::cout<<"\nThank You !";
+                        std::cin.ignore();
+                        std::cin.get();
+                        exit(1);
+                    }
+
+
+                }else{
+                    std::cout << "[-] Username or Password invalied";
+                }
                 return 0;
             }
 
@@ -179,8 +313,8 @@ class Cafeteria
                 {
                     order();
                 }else if (selected == 2){
-                    //login();
-                    std::cout<< "Login is Under construction";
+                    login();
+                    //std::cout<< "Login is Under construction";
                     exit(0);
                 }else if (selected ==3){
                     std::cout << "\nThank You!, Come again.\n";
@@ -194,13 +328,15 @@ class Cafeteria
        
 };
 
+
+
 int main()
 {
     DB db;
     db.connect();
-    Cafeteria cafe(db);
+    Admin admin;
+    Cafeteria cafe(db,admin);
     cafe.ops();
 
     return 0;
 }
-
